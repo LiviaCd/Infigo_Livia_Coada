c47af444734837009074ece2efefb23f6be57ae4
Livia Coadă <120571940+LiviaCd@users.noreply.github.com>
2025-02-01 02:23:51 +0200

    Add_migration_update_comment_table

---
+++ CMSPlus.Domain/Migrations/V1_1_4__Update_Value_Comment_Table.sql
@@ -0,0 +1,12 @@
+﻿USE [CMSPlus]
+GO
+SET ANSI_NULLS ON
+    GO
+    SET QUOTED_IDENTIFIER ON
+    GO
+UPDATE [dbo].[Comments] 
+SET [Comment] = '' 
+WHERE [Comment] IS NULL;
+
+ALTER TABLE [dbo].[Comments] 
+ALTER COLUMN [Comment] [nvarchar](max) NOT NULL;
